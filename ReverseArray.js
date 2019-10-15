7b534692a1a14e7c9d990e9b129cf5395d543889
CuongPV <pvcd03cntt@gmail.com>
2019-10-15 10:06:19 +0700

    Reverse array

---
+++ ReverseArray.js
@@ -0,0 +1,16 @@
+//How do you reverse an array
+function reverseArr(arrInput,start,end){
+    if (start > end) 
+    return; 
+    let temp = arrInput[start];  
+    arrInput[start] = arrInput[end]; 
+    arrInput[end] = temp; 
+      
+    // Recursive Function calling 
+    reverseArr(arrInput, start + 1, end - 1);
+   
+}
+let arr = [1,2,3,3,8,9,7];
+console.log(`Array input : ${arr}`);
+reverseArr(arr,0,arr.length -1);
+console.log(`Reversed array is : ${arr}`);
